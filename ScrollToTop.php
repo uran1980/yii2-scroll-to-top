43d8a4c5aaad0a486ddc35f4d4c8b13a37d414ca
uran1980 <uran1980@gmail.com>
2015-03-09 11:47:00 +0200

    fix

--- ScrollToTop.php
+++ ScrollToTop.php
@@ -6,12 +6,16 @@ use yii\helpers\Html;
 
 class ScrollToTop extends \yii\base\Widget
 {
+    public $isRegistered = false;
+
     /**
      * @inheritdoc
      */
     public function init()
     {
         $view = $this->getView();
+        $bandles = $view->getAssetManager()->bundles;
+        $this->isRegistered = isset($bandles['uran1980\\yii\\widgets\\scrollToTop\\ScrollToTopAsset']) ?: false;
         AppScrollToTopAsset::register($view);
     }
 
@@ -20,10 +24,12 @@ class ScrollToTop extends \yii\base\Widget
      */
     public function run()
     {
-        return Html::a('', null, [
-            'class' => 'scroll-to-top-link',
-            'style' => 'display: none;',
-            'href'  => '#wrapper',
-        ]);
+        if ( false === $this->isRegistered ) {
+            return Html::a('', null, [
+                'class' => 'scroll-to-top-link',
+                'style' => 'display: none;',
+                'href'  => '#wrapper',
+            ]);
+        }
     }
 }
